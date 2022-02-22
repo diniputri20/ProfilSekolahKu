78571750e9f2fcb013abe5317864d0895f222729
afwanaditya <afwanaditya2005@gmail.com>
2022-02-22 13:44:58 +0700

    b

--- app/src/main/java/com/example/profilsekolahku/MainActivity.java
+++ app/src/main/java/com/example/profilsekolahku/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.profilsekolahku;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+
+import com.example.profilsekolahku.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }public void profile(View view) {
+        Intent intent = new Intent(MainActivity.this, profilActivity.class);
+        startActivity(intent);
+    }
+
+    public void galeri(View view) {
+        Intent intent = new Intent(MainActivity.this, galeryActivity.class);
+        startActivity(intent);
+    }
+
+    public void ekskul(View view) {
+        Intent intent = new Intent(MainActivity.this, ekskulActivity.class);
+        startActivity(intent);
+    }
+
+    public void perpus(View view) {
+        Intent intent = new Intent(MainActivity.this, perpusActivity.class);
+        startActivity(intent);
+    }
+
+    public void guru(View view) {
+        Intent intent = new Intent(MainActivity.this, guruActivity.class);
+        startActivity(intent);
+    }
+
+    public void fasilitas(View view) {
+        Intent intent = new Intent(MainActivity.this, fasilitasActivity.class);
+        startActivity(intent);
     }
 }
