08d38037a7f1d2f020b8eaef9ba85f02a2d7aefe
diniputri20 <putridini509@gmail.com>
2022-02-24 12:16:35 +0700

    profil sekolahku

--- app/src/main/java/com/example/profilsekolahku/Buku7Activity.java
+++ app/src/main/java/com/example/profilsekolahku/Buku7Activity.java
@@ -15,21 +15,21 @@ public class Buku7Activity extends AppCompatActivity {
         setContentView(R.layout.activity_buku7);
     }
 
-    public void mtk(View view) {
-        goToUrl("https://drive.google.com/file/d/17zaH5O0YRtMNueKxCKeNv2U1_An7QOiK/view?usp=drivesdk");
+    public void mtk7(View view) {
+        goTourl("https://drive.google.com/file/d/17zaH5O0YRtMNueKxCKeNv2U1_An7QOiK/view?usp=drivesdk");
     }
 
-    public void bi(View view) {
-        goToUrl("https://drive.google.com/file/d/17i5FpSXODOm_j3SBltygrub5mRmlCLS1/view?usp=drivesdk");
+    public void bi7(View view) {
+        goTourl("https://drive.google.com/file/d/17i5FpSXODOm_j3SBltygrub5mRmlCLS1/view?usp=drivesdk");
+
     }
 
-    public void ipa(View view) {
-        goToUrl("https://drive.google.com/file/d/17ohLAJooXRxPksCppQPFyVcmzS0v64xS/view?usp=drivesdk");
+    public void big7(View view) {
+        goTourl("https://drive.google.com/file/d/17ohLAJooXRxPksCppQPFyVcmzS0v64xS/view?usp=drivesdk");
     }
-
-    private void goToUrl (String url) {
+    private void goTourl(String url){
         Uri uriUrl = Uri.parse(url);
-        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-        startActivity(launchBrowser);
+        Intent LauncherBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(LauncherBrowser);
     }
 }
