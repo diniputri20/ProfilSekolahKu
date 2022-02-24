35affb35d88ad38891471dbdaefd7867c9df2bb3
diniputri20 <putridini509@gmail.com>
2022-02-24 08:32:00 +0700

    profil sekolahku

--- app/src/main/java/com/example/profilsekolahku/ProfilSekolahKuActivity.java
+++ app/src/main/java/com/example/profilsekolahku/ProfilSekolahKuActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class ProfilSekolahKuActivity extends AppCompatActivity {
+    private PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil_sekolah_ku);
+
+        pdfView = (PDFView) findViewById(R.id.pdfView);
+
+        pdfView.fromAsset("profil.pdf").load();
     }
 }
