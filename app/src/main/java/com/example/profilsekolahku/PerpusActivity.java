25bf9949bc464df9b5d870feea3ecd5f75a7b063
diniputri20 <putridini509@gmail.com>
2022-02-25 09:15:03 +0700

    profil sekolahku

--- app/src/main/java/com/example/profilsekolahku/PerpusActivity.java
+++ app/src/main/java/com/example/profilsekolahku/PerpusActivity.java
@@ -11,27 +11,11 @@ import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
 public class PerpusActivity extends AppCompatActivity {
-    SliderView sliderView;
-    int[] images = {R.drawable.ekskul1,
-            R.drawable.eskul3,
-            R.drawable.ekskul1,
-            R.drawable.eskul3,
-            R.drawable.ekskul1};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perpus);
-
-        sliderView = findViewById(R.id.image_slider);
-
-        SliderAdapter sliderAdapter = new SliderAdapter(images);
-
-        sliderView.setSliderAdapter(sliderAdapter);
-        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
-        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
-        sliderView.startAutoCycle();
     }
 
     public void Buku7(View view) {
